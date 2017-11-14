68f60af44ba3a67d3a60494e77f48c7f9ef9f537
Eirini Kolimatsi <32462023+ekolima@users.noreply.github.com>
2017-11-15 00:27:07 +0200

    Update Suggestion1.java

--- Suggestion1.java
+++ Suggestion1.java
@@ -1,10 +1,10 @@
 // len_s and len_t are the number of characters in string s and t respectively
 public class Suggestion1 {
-	public int LevenshteinDistance(String s, int len_s, String t, int len_t) {
+
+	public int LevenshteinDistance(String s, String t, int len_s, int len_t) {
 		int cost;
 		int result = -1;
 		int j = Math.max(len_s,len_t);
-
 		if (len_s ==0 || len_t == 0) {
 			if (len_s == 0) {
 				result = len_t;
@@ -12,20 +12,17 @@ public class Suggestion1 {
 				result = len_s;
 			}
 		} else {
-
 			for (int i = 0; i < j; i++) {
 				if (s.substring(len_s-1) == t.substring(len_t-1)) {
 					cost = 0;
-					} else {
+				} else {
 					cost = 1;
-					}
-				result = Math.min(Math.min(LevenshteinDistance(s, len_s - 1, t, len_t) + 1,
-                 LevenshteinDistance(s, len_s    , t, len_t - 1) + 1),
-                 LevenshteinDistance(s, len_s - 1, t, len_t - 1) + cost);
-                }
-			}
-
-
+				}
+				result = Math.min(Math.min(LevenshteinDistance(s, t, len_s - 1, len_t) + 1, 
+						LevenshteinDistance(s, t, len_s, len_t - 1) + 1), 
+						LevenshteinDistance(s, t, len_s - 1, len_t - 1) + cost);
+	         }
+		}
 		return result;
 	}
 }
