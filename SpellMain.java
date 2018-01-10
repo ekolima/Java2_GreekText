c34cd255fb16630440c0e6239d86ee19d57e2c9c
geokar10 <32622170+geokar10@users.noreply.github.com>
2018-01-10 11:29:38 +0200

    Update SpellMain.java

--- SpellMain.java
+++ SpellMain.java
@@ -1,16 +1,55 @@
 import java.util.*;
 
+/**
+ * @author Team Jarvellous.exe
+ *         <p>
+ *         The Spell Checker software gets text input from the user and has the
+ *         ability to check for mistakes and propose suggestions for words with
+ *         mistakes. It supports 3 languages:
+ *         <p>
+ *         - Greek
+ *         <p>
+ *         - Romanian
+ *         <p>
+ *         Moreover, it includes a grading tool, which actually helps user
+ *         understand the quality of the given text.
+ *         <p>
+ *         <p>
+ *         ---------------------------------------
+ *         <p>
+ *         The {@code SpellMain} class combines all the other classes, we have
+ *         developed, to make the spell checker run.
+ */
 public class SpellMain {
-	
+
+	/**
+	 * The {@code main} method is the only method of {@code SpellMain} class and is
+	 * the heart of our programme, as it makes it produce results.
+	 * <p>
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		//create dictionary
-		ArrayList<String> dictionary= DictionaryGenerator.createDictionary();
-		//create extra dictionary
-		ArrayList<keyObject> extraDict = DictionaryGenerator.extraArrayList(dictionary);
-		//create soundex dictionary
-		ArrayList<String> dictSoundEx = DictionaryGenerator.createSoundExDictionary(dictionary);
+		System.out.println("-----------------------------------------");
+		System.out.println("------ Welcome to Spellchecker!!! -------");
+		System.out.println("-----------------------------------------");
+		System.out.println("------------ Step 1: Language -----------");
+		System.out.println("-----------------------------------------");
+		HashMap<Integer, String> dictionary = DictionaryGenerator.dictionary();
 		ArrayList<String> givenText = new ArrayList<String>();
-		givenText = inputFromTXT.textInput();
-		WordSearch.runWS(givenText, dictionary, extraDict, dictSoundEx); //soundExDict to be created!
+		System.out.println("-----------------------------------------");
+		System.out.println("-------- Step 2: Input your text --------");
+		System.out.println("-----------------------------------------");
+		int choice = inputFromTXT.dialogueInput();
+		givenText = inputFromTXT.textInput(choice);
+		System.out.println("-----------------------------------------");
+		System.out.println("-------- Step 3: Spelling Check ---------");
+		System.out.println("-----------------------------------------");
+		WordSearch.runWS(givenText, dictionary);
+		System.out.println("-----------------------------------------");
+		System.out.println("Thank you very much for using our software");
+		System.out.println("-----------------------------------------");
+		System.out.println("----- Development : Jarvellous.exe ------");
+		System.out.println("-----------------------------------------");
 	}
 }
