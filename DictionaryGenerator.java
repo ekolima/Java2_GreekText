a8d04fd6d45669cf19d661fd213c804bebaf7f25
Eirini Kolimatsi <32462023+ekolima@users.noreply.github.com>
2018-01-10 07:22:51 +0200

    Update DictionaryGenerator.java

--- DictionaryGenerator.java
+++ DictionaryGenerator.java
@@ -2,12 +2,12 @@ import java.io.*;
 import java.util.*;
 
 /**
- * @author Danay Kape
- * @author Eirini Kolimatsi
+ * @author Team jarvellous.exe
  *         <p>
  *         The {@code DictionaryGenerator} class asks the user to pick a
- *         language (Greek, English and Italian) and then generates for him or her a HashMap dictionary,
- *         based on which the spell check will be done.
+ *         language (Greek and Romanian) and then generates for him or
+ *         her a HashMap dictionary, based on which the spell check will be
+ *         done.
  */
 public class DictionaryGenerator {
 
@@ -21,17 +21,14 @@ public class DictionaryGenerator {
 	public static HashMap<Integer, String> dictionary() {
 		String language;
 		System.out.println("1 - Greek");
-		System.out.println("2 - English");
-		System.out.println("3 - Italian");
+		System.out.println("2 - Romanian");
 		System.out.println("0 - Exit");
 		System.out.println("Choose text language, by entering its code as mentioned above: ");
-		int languageChoice = WordSearch.getChoice(3);
+		int languageChoice = WordSearch.getChoice(2);
 		if (languageChoice == 1) {
 			language = "el_GRDict4.txt";
 		} else if (languageChoice == 2) {
-			language = "en_ENDict.txt";
-		} else if (languageChoice == 3) {
-			language = "it_ITDict.txt";
+			language = "ro_RODict.txt";
 		} else {
 			language = null;
 			System.out.println("-----------------------------------------");
@@ -60,7 +57,7 @@ public class DictionaryGenerator {
 			// when i = 0, the element with key =0 was null. This way it's ok.
 			int i = -1;
 			BufferedReader reader = new BufferedReader(
-					new InputStreamReader(new FileInputStream("src/".concat(fileName)), "ISO-8859-7"));
+					new InputStreamReader(new FileInputStream("src/main/resources/".concat(fileName)), "ISO-8859-7"));
 			String line;
 			while ((line = reader.readLine()) != null) {
 				i++;
